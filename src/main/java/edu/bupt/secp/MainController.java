d88bc65b1bcadb37d4dd7f5d9b17554ea772d1bb
liangxun <liangxun21@gmail.com>
2018-11-26 23:36:00 +0800

    添加注释：区分@Controller和@RestController

--- src/main/java/edu/bupt/secp/MainController.java
+++ src/main/java/edu/bupt/secp/MainController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+
+// @Controller注解下将返回HTML，比如在home方法中的return就默认是去template文件夹下面找homePage.html
 @Controller
 public class MainController {
 
--- src/main/java/edu/bupt/secp/hello.java
+++ src/main/java/edu/bupt/secp/hello.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+
+//@RestController注解下将返回json格式的数据，比如hello方法的return就直接返回"hello springboot and java"到浏览器
 @RestController
 public class hello {
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
