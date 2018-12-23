dafd29ff4f3a0447d5c06e2f43c484c4d8e864ed
liangxun <liangxun21@gmail.com>
2018-12-23 19:45:45 +0800

    显示在线人数

--- src/main/java/edu/bupt/secp/MainController.java
+++ src/main/java/edu/bupt/secp/MainController.java
@@ -1,13 +1,34 @@
+/*
+* 后台管理模块
+* 可以显示当前在线人数、在线商家等统计数据
+* */
 package edu.bupt.secp;
 
+import edu.bupt.secp.model.User;
+import edu.bupt.secp.model.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
+@RequestMapping(value = "admin")
 public class MainController {
 
     @RequestMapping(value = "/homePage")
     public String home(){
         return "homePage";
+    }
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @RequestMapping(value="userlist", method = RequestMethod.GET)
+    public String users_static(Model model){
+        model.addAttribute("users", userRepository.findAll());
+        model.addAttribute("count", userRepository.count());
+        return "userstatic";
     }
 }
