64c2b4fbb6efd88dd9e45c117fd80ae0aeb36fd2
moranandlinxi <951624452@qq.com>
2018-12-23 17:47:51 +0800

    display restaurant and meau

--- src/main/java/edu/bupt/secp/DisplayController.java
+++ src/main/java/edu/bupt/secp/DisplayController.java
@@ -1,4 +1,27 @@
 package edu.bupt.secp;
 
-public class DisplayController {
+import edu.bupt.secp.model.Meau;
+import edu.bupt.secp.model.Restaurant;
+import edu.bupt.secp.model.RestaurantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import edu.bupt.secp.model.MeauRepository;
+
+import java.util.List;
+
+public class DisplayController{
+    @Autowired
+    private MeauRepository meauRepository;
+    private RestaurantRepository restaurantRepository;
+
+    @RequestMapping(value="displaymeau", method = RequestMethod.GET)
+    public List<Meau> all_meau(){
+        return  (List<Meau>)meauRepository.findAll();
+    }
+
+    @RequestMapping(value="displayrestaurant", method = RequestMethod.GET)
+    public List<Restaurant> all_restaurant(){
+        return  (List<Restaurant>) restaurantRepository.findAll();
+    }
 }
