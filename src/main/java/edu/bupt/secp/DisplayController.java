78d08fa0e3b583e1e77bf30501a845bc9ec319e2
liangxun <liangxun21@gmail.com>
2018-12-23 20:34:25 +0800

    显示菜品

--- src/main/java/edu/bupt/secp/DisplayController.java
+++ src/main/java/edu/bupt/secp/DisplayController.java
@@ -1,25 +1,28 @@
 package edu.bupt.secp;
 
-import edu.bupt.secp.model.Meau;
-import edu.bupt.secp.model.Restaurant;
-import edu.bupt.secp.model.RestaurantRepository;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import edu.bupt.secp.model.Meau;
+import edu.bupt.secp.model.Restaurant;
+import edu.bupt.secp.model.RestaurantRepository;
 import edu.bupt.secp.model.MeauRepository;
 
-import java.util.List;
 
+@RestController
+@RequestMapping(value="/display")
 public class DisplayController{
     @Autowired
     private MeauRepository meauRepository;
-    private RestaurantRepository restaurantRepository;
-
+    
     @RequestMapping(value="displaymeau", method = RequestMethod.GET)
-    public List<Meau> all_meau(){
-        return  (List<Meau>)meauRepository.findAll();
+    public List<Meau> all_meau(@RequestParam Integer restaurant_id){
+        return meauRepository.findMeausByRestaurant_id(restaurant_id);
     }
 
+    @Autowired
+    private RestaurantRepository restaurantRepository;
     @RequestMapping(value="displayrestaurant", method = RequestMethod.GET)
     public List<Restaurant> all_restaurant(){
         return  (List<Restaurant>) restaurantRepository.findAll();
--- src/main/java/edu/bupt/secp/model/MeauRepository.java
+++ src/main/java/edu/bupt/secp/model/MeauRepository.java
@@ -1,7 +1,13 @@
 package edu.bupt.secp.model;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import edu.bupt.secp.model.Meau;
 
+import java.util.List;
+
 public interface MeauRepository extends CrudRepository<Meau, Iterable> {
+
+    @Query(value = "select * from meau where restaurant_id = ?1", nativeQuery = true)
+    List<Meau> findMeausByRestaurant_id(Integer n);
 }
